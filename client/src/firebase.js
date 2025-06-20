4acdff99d67bbdb9f2931118a9568921c9302446
Shresth Kaushik <107255313+Shresth-kaushik@users.noreply.github.com>
2025-06-20 14:48:33 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey:
-  authDomain:
-  projectId: 
-  storageBucket: 
-  messagingSenderId: 
-  appId: 
-  measurementId: 
+  apiKey: "AIzaSyDuySxaDN1XkYL1rJ3hHOpeZWptjxKADs8",
+  authDomain: "pollingsystem-56c6b.firebaseapp.com",
+  projectId: "pollingsystem-56c6b",
+  storageBucket: "pollingsystem-56c6b.firebasestorage.app",
+  messagingSenderId: "595877005525",
+  appId: "1:595877005525:web:18357b2ccfb3169273ba5c",
+  measurementId: "G-K49SVJHDBG"
 };
 
 const app = initializeApp(firebaseConfig);
